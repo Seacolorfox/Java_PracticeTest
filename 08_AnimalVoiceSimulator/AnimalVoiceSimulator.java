83a7aedeaf302c530991adddb7dc8396ba307d02
Seacolorfox <Seacolorfox@outlook.com>
2019-06-09 19:34:55 +0800

    简单完成09

--- 08_AnimalVoiceSimulator/AnimalVoiceSimulator.java
+++ 08_AnimalVoiceSimulator/AnimalVoiceSimulator.java
@@ -1,4 +1,48 @@
+interface Animal
+{
+    public abstract void cry();
+    public abstract String getAnimalName();
+}
+class Simulator
+{
+    public void playSound(Animal animal)
+    {
+        System.out.println("NAME:"+animal.getAnimalName());
+        animal.cry();
+    }
+}
+class Cat implements Animal
+{
+    String name = "小猫Tom";
+    @Override
+    public void cry() {
+        System.out.println("小猫喵喵叫");
+    }
+
+    @Override
+    public String getAnimalName() {
+        return name;
+    }
+}
+    class Dog implements Animal
+{
+    String name = "小狗Spike";
+    @Override
+    public void cry() {
+        System.out.println("小狗汪汪叫");
+    }
+
+    @Override
+    public String getAnimalName() {
+        return name;
+    }
+}
 public class AnimalVoiceSimulator
 {
-
+    public static void main(String args[])
+    {
+        Simulator simulator= new Simulator();
+        simulator.playSound(new Dog());
+        simulator.playSound(new Cat());
+    }
 }
