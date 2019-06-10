ee8e86ff5009269d40355bfe5739e2c6ec6322c3
Seacolorfox <Seacolorfox@outlook.com>
2019-06-10 22:38:30 +0800

    Update FenJie.java

--- 04_FenJie/FenJie.java
+++ 04_FenJie/FenJie.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
+
 public class FenJie
 {
     public static void main(String args[])
     {
-
+        int n = new Scanner(System.in).nextInt();
+        System.out.print(n+"=");
+        for(int k = 2;k<=n;)
+        {
+            if(n==k)
+            {
+                System.out.print(k);
+                break;
+            }
+            else
+                if(n%k==0)
+                {
+                    System.out.print(k+"*");
+                    n/=k;
+                }
+                else k++;
+        }
     }
 }
+//  while (k<=n)
+//  {
+//        if(k==n)
+//        {
+//           System.out.print(n);
+//           break;
+//        }
+//        else
+//          if (n%k==0)
+//          {
+//              System.out.print(k+"*");
+//               n=n/k;
+//           }
+//           else k++;
+//   }
